2516bd6bd6eb4bcaeb2815bf6c56334a3d39aa70
jaw0094 <jaw0094@hotmail.com>
2022-08-25 23:15:07 -0500

    cambio

---
+++ gestion/src/main/java/udea/edu/co/sistemagestion/gestion/GestionApplication.java
@@ -0,0 +1,13 @@
+package udea.edu.co.sistemagestion.gestion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionApplication.class, args);
+	}
+
+}
---
+++ gestion/src/test/java/udea/edu/co/sistemagestion/gestion/GestionApplicationTests.java
@@ -0,0 +1,13 @@
+package udea.edu.co.sistemagestion.gestion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
