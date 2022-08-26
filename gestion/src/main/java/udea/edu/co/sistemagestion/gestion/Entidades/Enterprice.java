025ad23e6e881105ca7b154e97c4d195b3c1d301
jaw0094 <jaw0094@hotmail.com>
2022-08-25 23:15:07 -0500

    Creacion entidad empresa

--- gestion/src/main/java/udea/edu/co/sistemagestion/gestion/Entidades/Enterprice.java
+++
@@ -1,4 +0,0 @@
-package udea.edu.co.sistemagestion.gestion.Entidades;
-
-public class Enterprice {
-}
---
+++ gestion/src/main/java/udea/edu/co/sistemagestion/gestion/Entidades/Enterprise.java
@@ -0,0 +1,86 @@
+package udea.edu.co.sistemagestion.gestion.Entidades;
+import java.util.Date;
+
+public class Enterprise {
+
+    private long id;
+    private String name;
+    private String document;
+    private String phone;
+    private String address;
+    private user;
+    private transactions;
+    private Date createdAt;
+    private Date updatedAt;
+
+    public Enterprise() {
+
+    }
+
+    public Enterprise(long id, String name, String document, String phone, String address, Date createdAt, Date updatedAt) {
+        this.id = id;
+        this.name = name;
+        this.document = document;
+        this.phone = phone;
+        this.address = address;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDocument() {
+        return document;
+    }
+
+    public void setDocument(String document) {
+        this.document = document;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+}
