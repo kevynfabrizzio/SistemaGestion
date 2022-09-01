34a7a0846b883f42acb3f4034123a412408e01b7
Anyi91 <111194367+Anyi91@users.noreply.github.com>
2022-08-31 22:05:43 -0500

    Avance controlador - prueba OK Get

--- gestion/src/main/java/udea/edu/co/sistemagestion/gestion/Controladores/EnterpriseController.java
+++ gestion/src/main/java/udea/edu/co/sistemagestion/gestion/Controladores/EnterpriseController.java
@@ -1,19 +1,23 @@
 package udea.edu.co.sistemagestion.gestion.Controladores;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import udea.edu.co.sistemagestion.gestion.Entidades.Employee;
 
 @RestController
 @RequestMapping("/enterprises")
 public class EnterpriseController {
 
-    @GetMapping(value="/listar")
-
+    @GetMapping(value = "/listar")
     public String listEnterprise() {
         return "Esto es una prueba";
 
     }
 
+    @PostMapping(value = "/guardar")
+    public ResponseEntity<Employee> guardar(@RequestBody Employee empleado) {
+        return new ResponseEntity<Employee>(empleado, HttpStatus.OK);
+
+    }
 }
