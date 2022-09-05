3bce5dc3fdeaec00e3110faffbc4a6f4b6bd92b3
GTavx <tavalmuro@gmail.com>
2022-09-05 00:24:34 -0500

    El sistema retorna response 200 en las ruta users y user/{id} con GET,POST,PATCH Y DELETE

--- gestion/src/main/java/udea/edu/co/sistemagestion/gestion/Controladores/EmployeeController.java
+++ gestion/src/main/java/udea/edu/co/sistemagestion/gestion/Controladores/EmployeeController.java
@@ -1,29 +1,67 @@
 package udea.edu.co.sistemagestion.gestion.Controladores;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import udea.edu.co.sistemagestion.gestion.Entidades.*;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
 
 @RestController
-@RequestMapping("/Employee")
+@RequestMapping("/users")
 public class EmployeeController {
     ArrayList employees=new ArrayList();
     Profile prof;
-    @GetMapping(value = "/users")
+    @GetMapping
     public ArrayList employees() {
         return employees;
     }
 
-    @PostMapping(value = "/users")
+    @PostMapping
     public ArrayList savex() {
         prof=new Profile();prof.setId("3");
         Employee emp=new Employee();emp.setId(employees.size());emp.setName("ramiro");emp.setProfile(prof);
         employees.add(emp);
         return employees;
+    }
+
+    @GetMapping("/user/{id}")//@GetMapping(value="/user/{id}") //para busquedas por un solo parametro
+    public Employee findemployeeId(@PathVariable int id)
+    {
+        Employee empx=null;
+        for (Object emp : employees) {
+            empx=(Employee)emp;
+            if(empx.getId()==id){
+                break;
+            }else{empx=null;}
+        }
+        return empx; //serviciosEmployee.buscarEmployee(id);
+    }
+
+    @PatchMapping("/user/{id}")
+    public Employee upemployeeId(@PathVariable int id)
+    {
+        Employee empx=null;
+        for (Object emp : employees) {
+            empx=(Employee)emp;
+            if(empx.getId()==id){
+                empx.setName("ramon valdez");
+                break;
+            }else{empx=null;}
+        }
+        return empx; //serviciosEmployee.buscarEmployee(id);
+    }
+
+    @DeleteMapping("/user/{id}")
+    public ArrayList delemployeeId(@PathVariable int id)
+    {
+        Employee empx=null;
+        for (Object emp : employees) {
+            empx=(Employee)emp;
+            if(empx.getId()==id){
+                employees.remove(empx);
+                break;
+            }
+        }
+        return employees; //serviciosEmployee.buscarEmployee(id);
     }
 }
