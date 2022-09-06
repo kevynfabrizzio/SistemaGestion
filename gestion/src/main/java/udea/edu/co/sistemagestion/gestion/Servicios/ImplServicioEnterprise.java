1c98994a3747c9d0549a91587b3e85a009cb5476
jaw0094 <jaw0094@hotmail.com>
2022-09-06 13:42:14 -0500

    importaciones

--- gestion/src/main/java/udea/edu/co/sistemagestion/gestion/Servicios/ImplServicioEnterprise.java
+++ gestion/src/main/java/udea/edu/co/sistemagestion/gestion/Servicios/ImplServicioEnterprise.java
@@ -1,10 +1,19 @@
 package udea.edu.co.sistemagestion.gestion.Servicios;
 
 import udea.edu.co.sistemagestion.gestion.Entidades.Enterprise;
+import udea.edu.co.sistemagestion.gestion.Repositorio.IRepositorioEnterprise;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class ImplServicioEnterprise implements IServicioEnterprise{
+
+
+
+    @Autowired
+    IServicioEnterprise repositoryEnterprise;
 
     @Override
     public List<Enterprise> getAll() {
